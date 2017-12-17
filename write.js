ce989e4156b76336138b2503293eef6c8a1d49cd
death667b <idaniel@spinnys.com>
2017-12-17 05:00:16 +0000

    Update write.js

--- write.js
+++ write.js
@@ -17,7 +17,7 @@ function write() {
         return letter.map(line => line.toString(2));
     }
 
-    const a = binary(font.a);
+    const a = binary(font.b);
     
     a.forEach(line=>
         magik.dixit(line.padStart(8,"0"))
